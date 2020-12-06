28a1a0c23c9f2d91fc3296c792c02975a06fc247
know-how1997 <56219485+know-how1997@users.noreply.github.com>
2020-12-06 12:28:07 +0400

    tasks

---
+++ BracketValidation/BracketValidation/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BracketValidation
+{
+    class Program
+    {
+        public static void Main(String[] args)
+        {
+            while (true)
+            {
+                string input = Console.ReadLine();
+
+                Console.WriteLine(AreBracketsCorrectlyDisplayed(input));
+            }
+        }
+        static bool IsMatchingPair(char char1, char char2)
+        {
+            if (char1 == '(' && char2 == ')')
+                return true;
+            else if (char1 == '[' && char2 == ']')
+                return true;
+            else
+                return false;
+        }
+
+        static bool AreBracketsCorrectlyDisplayed(string input)
+        {
+            List<char> ls = new List<char>();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i] == '('
+                    || input[i] == '[')
+                    ls.Add(input[i]);
+                if (input[i] == ')'
+                    || input[i] == ']')
+                {
+                    if (ls.Count == 0)
+                    {
+                        return false;
+                    }
+                    else if (!IsMatchingPair(ls.LastOrDefault(), input[i]))
+                    {
+                        return false;
+                    }
+                    ls.RemoveAt(ls.Count - 1);
+                }
+            }
+            if (ls.Count == 0)
+                return true;
+            else
+            {
+                return false;
+            }
+        }
+
+
+    }
+}
+
+
---
+++ CountIslands/CountIslands/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace CountIslands
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            int[,] matrix = new int[n, n];
+
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                    matrix[i, j] = new Random().Next(0, 2);
+            }
+
+
+            Console.WriteLine($"number of islands : {GetNumberOfIslands(matrix, n)}");
+        }
+        static int GetNumberOfIslands(int[,] matrix, int n)
+        {
+            bool[,] isVisited = new bool[n, n];
+            int count = 0;
+            for (int i = 0; i < n; i++)
+                for (int j = 0; j < n; j++)
+                    if (matrix[i, j] == 1 && !isVisited[i, j])
+                    {
+                        LandCheck(matrix, i, j, n, isVisited);
+                        count++;
+                    }
+            return count;
+        }
+        static void LandCheck(int[,] matrix, int i, int j, int n, bool[,] isVisited)
+        {
+            int[] horizontal = new int[] { -1, -1, -1, 0, 0, 1, 1, 1 };
+            int[] vertical = new int[] { -1, 0, 1, -1, 1, -1, 0, 1 };
+
+            isVisited[i, j] = true;
+            for (int k = 0; k < 8; k++)
+                if (IsNewLand(matrix, i + horizontal[k], j + vertical[k], n, isVisited))
+                    LandCheck(matrix, i + horizontal[k], j + vertical[k], n, isVisited);
+        }
+        static bool IsNewLand(int[,] matrix, int i, int j, int n, bool[,] isVisited)
+        {
+            return (i >= 0) && (i < n) && (j >= 0) && (j < n) && (matrix[i, j] == 1 && !isVisited[i, j]);
+        }
+    }
+}
+
+
